3f86e611730f760bc57328c208c39e8d20ce0b70
Vikramaditya Kokil <vk92kokil@gmail.com>
2025-01-30 12:11:54 +0530

    fix test

--- test.js
+++ test.js
@@ -1,41 +1,24 @@
-const xhrMock = require('xhr-mock');
+const nock = require('nock');
 const test = require('ava');
-const validator = require('validator');
-const {ipv4, ipv6} = require('./index');
+const net = require('net');
+const {ipv4, ipv6} = require('./lib/index');
 
-const { default: mock, proxy } = xhrMock;
-
-test.beforeEach(t => {
-	mock.setup();
-	mock.use(proxy);
-});
+const scope = nock('https://api6.ipify.org')
+  .get('/')
+  .reply(200, 'd64d:387b:be1d:28cf:d1b5:c2d7:b6e2:829d')
 
 test('isipv4 server', async t => {
 	const ip = await ipv4();
-	console.log(ip);
-	t.true(validator.isIP(ip));
+	t.true(net.isIPv4(ip));
 });
 
 test('isipv6 server', async t => {
 	const ip = await ipv6();
-	console.log(ip);
-	t.true(validator.isIP(ip));
-});
-test('isipv4 browser', async t => {
-	const ip = await ipv4();
-	console.log(ip);
-	t.true(validator.isIP(ip));
-});
-
-test('isipv6 browser', async t => {
-	const ip = await ipv6();
-	console.log(ip);
-	t.true(validator.isIP(ip));
+	t.true(net.isIPv6(ip));
 });
 
 test('Custom IP api', async t => {
 	const body = await ipv6("https://postman-echo.com/ip");
 	const { ip } = JSON.parse(body);
-	console.log(ip);
-	t.true(validator.isIP(ip));
+	t.true(net.isIPv4(ip));
 });
